c5e2f03aa09842b582014a55810aad4e43ac9239
Felipe Conde <felipe_conde@atlantico.com.br>
2013-11-22 11:13:35 -0300

    including new methos on README and cleaning up the sample code

--- easyFoursquare4AndroidExample/src/br/com/condesales/MainActivity.java
+++ easyFoursquare4AndroidExample/src/br/com/condesales/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.location.Location;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,76 +18,32 @@ import br.com.condesales.listeners.TipsResquestListener;
 import br.com.condesales.listeners.UserInfoRequestListener;
 import br.com.condesales.models.Tip;
 import br.com.condesales.models.User;
-import br.com.condesales.tasks.tips.TipsNearbyRequest;
 import br.com.condesales.tasks.users.UserImageRequest;
 
 public class MainActivity extends Activity implements
-		AccessTokenRequestListener, ImageRequestListener {
+        AccessTokenRequestListener, ImageRequestListener {
 
-	private EasyFoursquareAsync async;
-	private ImageView userImage;
-	private ViewSwitcher viewSwitcher;
-	private TextView userName;
+    private EasyFoursquareAsync async;
+    private ImageView userImage;
+    private ViewSwitcher viewSwitcher;
+    private TextView userName;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		userImage = (ImageView) findViewById(R.id.imageView1);
-		viewSwitcher = (ViewSwitcher) findViewById(R.id.viewSwitcher1);
-		userName = (TextView) findViewById(R.id.textView1);
-		async = new EasyFoursquareAsync(this);
-		async.requestAccess(this);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userImage = (ImageView) findViewById(R.id.imageView1);
+        viewSwitcher = (ViewSwitcher) findViewById(R.id.viewSwitcher1);
+        userName = (TextView) findViewById(R.id.textView1);
+        //ask for access
+        async = new EasyFoursquareAsync(this);
+        async.requestAccess(this);
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
-	@Override
-	public void onError(String errorMsg) {
-		// TODO Do something with the error message
-		Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
-	}
-
-	@Override
-	public void onAccessGrant(String accessToken) {
-		// TODO with the access token you can perform any request to foursquare.
-		// example:
-		async.getUserInfo(new UserInfoRequestListener() {
-
-			@Override
-			public void onError(String errorMsg) {
-				// TODO Some error getting user info
-				Toast.makeText(MainActivity.this, errorMsg, Toast.LENGTH_LONG)
-						.show();
-			}
-
-			@Override
-			public void onUserInfoFetched(User user) {
-				// TODO OWww. did i already got user!?
-				if (user.getBitmapPhoto() == null) {
-					UserImageRequest request = new UserImageRequest(
-							MainActivity.this, MainActivity.this);
-					request.execute(user.getPhoto());
-				} else {
-					userImage.setImageBitmap(user.getBitmapPhoto());
-				}
-				userName.setText(user.getFirstName() + " " + user.getLastName());
-				viewSwitcher.showNext();
-				Toast.makeText(MainActivity.this, "Got it!", Toast.LENGTH_LONG)
-						.show();
-			}
-		});
-        //requestTipsNearby(accessToken);
-	}
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     private void requestTipsNearby(String accessToken) {
         Location loc = new Location("");
@@ -97,7 +52,7 @@ public class MainActivity extends Activity implements
 
         TipsCriteria criteria = new TipsCriteria();
         criteria.setLocation(loc);
-        TipsNearbyRequest request = new TipsNearbyRequest(this, new TipsResquestListener() {
+        async.getTipsNearby(new TipsResquestListener() {
 
             @Override
             public void onError(String errorMsg) {
@@ -108,13 +63,53 @@ public class MainActivity extends Activity implements
             public void onTipsFetched(ArrayList<Tip> tips) {
                 Toast.makeText(MainActivity.this, tips.toString(), Toast.LENGTH_LONG).show();
             }
-        },criteria);
-        request.execute(accessToken);
+        }, criteria);
+    }
+
+
+    @Override
+    public void onError(String errorMsg) {
+        // Do something with the error message
+        Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
     }
 
     @Override
-	public void onImageFetched(Bitmap bmp) {
-		userImage.setImageBitmap(bmp);
-	}
+    public void onAccessGrant(String accessToken) {
+        // with the access token you can perform any request to foursquare.
+        // example:
+        async.getUserInfo(new UserInfoRequestListener() {
+
+            @Override
+            public void onError(String errorMsg) {
+                // Some error getting user info
+                Toast.makeText(MainActivity.this, errorMsg, Toast.LENGTH_LONG)
+                        .show();
+            }
+
+            @Override
+            public void onUserInfoFetched(User user) {
+                // OWww. did i already got user!?
+                if (user.getBitmapPhoto() == null) {
+                    UserImageRequest request = new UserImageRequest(
+                            MainActivity.this, MainActivity.this);
+                    request.execute(user.getPhoto());
+                } else {
+                    userImage.setImageBitmap(user.getBitmapPhoto());
+                }
+                userName.setText(user.getFirstName() + " " + user.getLastName());
+                viewSwitcher.showNext();
+                Toast.makeText(MainActivity.this, "Got it!", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        //for another example uncomment line below:
+        //requestTipsNearby(accessToken);
+    }
+
+    @Override
+    public void onImageFetched(Bitmap bmp) {
+        userImage.setImageBitmap(bmp);
+    }
 
 }
